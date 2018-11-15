a09e51bf4d2964e03bdce6cfca89edca0b46bc17
kkw87 <kkwang14@post.harvard.edu>
2018-11-15 02:07:12 -0500

    Initial Commit

---
+++ Twittermenti/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Twittermenti
+//
+//  Created by Kevin Wang on 11/15/18.
+//  Copyright © 2018 Kevin Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
